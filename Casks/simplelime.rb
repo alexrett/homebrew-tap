e4dd91320dcf68293eb8999911dcd4f3188ffdf2
Aleksei Malikov <alexrettvm@gmail.com>
2026-05-04 00:48:57 +0200

    add simplelime cask

---
+++ Casks/simplelime.rb
@@ -0,0 +1,19 @@
+cask "simplelime" do
+  version "0.1.0"
+  sha256 "7a117f983a50ff6069350223462582b7facf83e49047e301494e7b91cbd861a9"
+
+  url "https://github.com/alexrett/simplelime/releases/download/v#{version}/SimpleLime.dmg"
+  name "SimpleLime"
+  desc "Scratch-first text editor with temporary buffers and AI assistance"
+  homepage "https://github.com/alexrett/simplelime"
+
+  depends_on macos: ">= :ventura"
+
+  app "SimpleLime.app"
+
+  zap trash: [
+    "~/Library/Application Support/SimpleLime",
+    "~/Library/Caches/com.whitehappypony.SimpleLime",
+    "~/Library/Preferences/com.whitehappypony.SimpleLime.plist",
+  ]
+end
