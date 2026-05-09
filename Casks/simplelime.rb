d33d410c604500fddbf70787b548dec344075cce
Aleksei Malikov <alexrettvm@gmail.com>
2026-05-10 00:45:54 +0200

    Update SimpleLime to 0.4.0

--- Casks/simplelime.rb
+++ Casks/simplelime.rb
@@ -1,6 +1,6 @@
 cask "simplelime" do
-  version "0.3.7"
-  sha256 "ddb3d5e725d25379b8f3508da30b6d269b3eded126ef0d769a14ffcaf3a0e8c6"
+  version "0.4.0"
+  sha256 "c7eeb669071d644a5fe69182bc20bba592921ae724d7a2945836bf6f86c0ae20"
 
   url "https://github.com/alexrett/simplelime/releases/download/v#{version}/SimpleLime.dmg"
   name "SimpleLime"
