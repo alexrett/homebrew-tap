df86d7da399ff90e8e5277140da62750d972d192
Aleksei Malikov <alexrettvm@gmail.com>
2026-09-02 11:04:18 +0200

    chore: update Window Burn to 0.3.1

--- Casks/window-burn.rb
+++ Casks/window-burn.rb
@@ -1,6 +1,6 @@
 cask "window-burn" do
-  version "0.3.0"
-  sha256 "0f82a601e7b26b6ac1242750a311a3e4eef355982503a47d00dfb66e133e1d34"
+  version "0.3.1"
+  sha256 "aa394b8222ce7abec7d8d4d68d84e79d6decc46d871afa8b520b8ddc963f202c"
 
   url "https://github.com/alexrett/window-burn/releases/download/v#{version}/WindowBurn.dmg"
   name "Window Burn"
